04b944b11ab4e4b4153532f39bcb8e543c51cc89
JLP-55 <jack.v.pekin@gmail.com>
2024-02-10 18:53:55 +1100

    All content will render to the SVG file. Shape classes have return relevant data Use conditional statement in inquirer response to select correct shape child class render method to call

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {Circle, Triangle, Square} = require("./library/shapes");
-// const {Shapes, allShapes} = require("./library/shapes");
-// const text = require("./library/text");
-// const path = require("path");
+const {
+    Circle,
+    Triangle, 
+    Square
+} = require("./library/shapes");
 
 const userQuestions = [
     {
@@ -31,53 +32,27 @@ const userQuestions = [
         name: "logoColour",
         message: "What colour should your logos shape be?"
     },
-] 
+]
 
 inquirer.prompt(userQuestions)
-.then((response) => JSON.stringify(response))
-.then((response) => {
+    .then((response) => {
 
-    console.log(response.toLowerCase());
+        // Set the variable "shape" to an empty value
+        let shape;
 
-    const shape = new Shapes(response.logoColour);
-    // allShapes();
-    console.log(shape);
+        // Reasign value of shape dependant on the users input.
+        if (response.logoShape === "Circle") {
+            // Here, shape will equal a new instance of the Circle class
+            shape = new Circle(response.logoColour, response.textColour, response.textContent);
+        } else if (response.logoShape === "Square") {
+            shape = new Square(response.logoColour, response.textColour, response.textContent);
+        } else {
+            shape = new Triangle(response.logoColour, response.textColour, response.textContent);
+        };
 
-    // const allItems = { userQuestions: { response }};
-    
-    // const responseArray = [
-    //     response.textContent,
-    //     response.textColour,
-    //     response.logoShape,
-    //     response.logoColour
-    // ];
+        //Write to the file.
+        fs.writeFile("./generated_logo/user_logo.svg", shape.render(), (err) => {
+            err ? console.log(err) : console.log("Generated logo.svg");
+        });
 
-    // const circle = new Circle (response.logoColour, response.logoShape);
-    // Circle.prototype.fill = function () {
-    //     return `<circle r="80" cx="150" cy="105" style="fill:${response.logoColour};stroke:black;stroke-width:3;"/>`;
-    // };
-    // console.log(circle.fill());
-
-    // const text = new Text (response.textColour, response.textContent);
-
-    // console.log(circle);
-    // console.log(...responseArray);
-    // console.log(...response);
-
-    // var userSelectedItems = () => {
-    //     console.log(response.textContent, response.textColour, response.logoShape, response.logoColour);
-    //     return response.textContent, response.textColour, response.logoShape, response.logoColour;
-    // };
-
-    // The spread operator for response doesn't work
-    // The spread operator for userSelectedItems doesn't work
-    // If replaced with response."something", then that individual user selection will be renderd to the newly generated logo.svg file
-                                                // Neither of the fs.writeFile functions work.
-    // fs.writeFile("./generated_logo/user_logo.svg", { ...response }, (err) => {
-    // fs.writeFile("./generated_logo/user_logo.svg", JSON.stringify({ ...responseArray }), (err) => {
-    // fs.writeFile("./generated_logo/user_logo.svg", JSON.stringify(circle, text), (err) => {
-    // fs.writeFile("./user_generated_logo/logo.svg", userSelectedItems(...response), (err) => {
-        // err ? console.log(err) : console.log("Generated logo.svg");
-    // });
-    // console.log({ ...responseArray[2]});
-})
+    });
--- library/shapes.js
+++ library/shapes.js
@@ -1,67 +1,52 @@
-//Constructor functions
-function Shapes(colour) {
-    this.colour = colour;
-};
+// Create a parent class.
+class Shape {
+    constructor(logoColour, textColour, textContent) {
+        this.logoColour = logoColour;
+        this.textColour= textColour;
+        this.textContent =  textContent
+    };
+}
 
-function Circle(shape) {
-    Shapes.call(this, colour, shape);
-    this.shape = shape;
-    
-    this.fill = function() {
-        return `<circle r="80" cx="150" cy="105" style="fill:${this.colour};stroke:black;stroke-width:3;"/>`;
+// Create child classes to inherit from the parent.
+class Circle extends Shape {
+    render() {
+        
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+
+        <circle r="80" cx="150" cy="105" style="fill:${this.logoColour};stroke:black;stroke-width:3;"/>
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" style="fill:${this.textColour};stroke:black;stroke-width:1;">${this.textContent}</text>
+
+        </svg>`
     };
 };
 
-function Triangle(shape) {
-    Shapes.call(this, colour, shape);
-    this.shape = shape;
-    
-    this.fill = function() {
-        return `<polygon points=" 150,10 270,155, 30,155" style="fill:${this.colour};stroke:black;stroke-width:3;"/>`;
+
+class Square extends Shape {
+    render() {
+        
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+
+        <rect x="72" y="30" width="150" height="150" style="fill:${this.logoColour};stroke:black;stroke-width:3;"/>
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" style="fill:${this.textColour};stroke:black;stroke-width:1;">${this.textContent}</text>
+        
+        </svg>`
     };
 };
 
-function Square(shape) {
-    Shapes.call(this, colour, shape);
-    this.shape = shape;
-    
-    this.fill = function() {
-        return `<rect x="72" y="30" width="150" height="150" style="fill:${this.colour};stroke:black;stroke-width:3;"/>`;
+class Triangle extends Shape {
+    render() {
+        
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+
+        <polygon points=" 150,10 270,160, 30,160" style="fill:${this.logoColour};stroke:black;stroke-width:3;"/>
+
+        <text x="150" y="125" font-size="60" text-anchor="middle" style="fill:${this.textColour};stroke:black;stroke-width:1;">${this.textContent}</text>
+
+        </svg>`
     };
 };
 
-module.exports = {Circle, Triangle, Square};
 
-
-// class Shapes {
-//     constructor(colour) {
-//         this.colour = colour;
-//     };
-
-//     circle() {
-//         return `<circle r="80" cx="150" cy="105" style="fill:${this.colour};stroke:black;stroke-width:3;"/>`;
-//     };
-
-//     triangle() {
-//         return `<polygon points=" 150,10 270,155, 30,155" style="fill:${this.colour};stroke:black;stroke-width:3;"/>`;
-//     };
-
-//     square() {
-//         return `<rect x="72" y="30" width="150" height="150" style="fill:${this.colour};stroke:black;stroke-width:3;"/>`;
-//     };
-// };
-
-// function allShapes() {
-//     if (response.logoShape === "Cirlce") {
-//         const circle = new Shapes(response.logoShape);
-//         circle.circle();
-//     } else if (response.logoShape === "Triangle") {
-//         const triangle = new Shapes(response.logoShape);
-//         triangle.triangle();
-//     } else if (response.logoShape === "Square") {
-//         const square = new Shapes(response.logoShape);
-//         square.square();
-//     };
-// };
-
-// module.exports = {Shapes, allShapes};
+module.exports = { Circle, Square, Triangle };
