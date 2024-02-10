dde123a72ee21b21bd98980a17b43f717dfc9e95
JLP-55 <jack.v.pekin@gmail.com>
2024-02-10 14:40:05 +1100

    some changes All I could figure out, still not sure if I am exporting things correctly?

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {Circle, Text} = require("./library/shapes");
+const {Circle, Triangle, Square} = require("./library/shapes");
+// const {Shapes, allShapes} = require("./library/shapes");
+// const text = require("./library/text");
+// const path = require("path");
 
 const userQuestions = [
     {
@@ -31,34 +34,40 @@ const userQuestions = [
 ] 
 
 inquirer.prompt(userQuestions)
-// .then((response) => JSON.stringify(response))
+.then((response) => JSON.stringify(response))
 .then((response) => {
-    // console.log(response);
+
+    console.log(response.toLowerCase());
+
+    const shape = new Shapes(response.logoColour);
+    // allShapes();
+    console.log(shape);
+
     // const allItems = { userQuestions: { response }};
     
-    const responseArray = [
-        response.textContent,
-        response.textColour,
-        response.logoShape,
-        response.logoColour
-    ];
+    // const responseArray = [
+    //     response.textContent,
+    //     response.textColour,
+    //     response.logoShape,
+    //     response.logoColour
+    // ];
 
-    const circle = new Circle (response.logoColour, response.logoShape);
-    Circle.prototype.fill = function () {
-        return `<circle r="80" cx="150" cy="105" style="fill:${response.logoColour};stroke:black;stroke-width:3;"/>`;
-    };
+    // const circle = new Circle (response.logoColour, response.logoShape);
+    // Circle.prototype.fill = function () {
+    //     return `<circle r="80" cx="150" cy="105" style="fill:${response.logoColour};stroke:black;stroke-width:3;"/>`;
+    // };
     // console.log(circle.fill());
 
-    const text = new Text (response.textColour, response.textContent);
+    // const text = new Text (response.textColour, response.textContent);
 
     // console.log(circle);
     // console.log(...responseArray);
     // console.log(...response);
 
-    var userSelectedItems = () => {
-        console.log(response.textContent, response.textColour, response.logoShape, response.logoColour);
-        return response.textContent, response.textColour, response.logoShape, response.logoColour;
-    };
+    // var userSelectedItems = () => {
+    //     console.log(response.textContent, response.textColour, response.logoShape, response.logoColour);
+    //     return response.textContent, response.textColour, response.logoShape, response.logoColour;
+    // };
 
     // The spread operator for response doesn't work
     // The spread operator for userSelectedItems doesn't work
@@ -66,87 +75,9 @@ inquirer.prompt(userQuestions)
                                                 // Neither of the fs.writeFile functions work.
     // fs.writeFile("./generated_logo/user_logo.svg", { ...response }, (err) => {
     // fs.writeFile("./generated_logo/user_logo.svg", JSON.stringify({ ...responseArray }), (err) => {
-    fs.writeFile("./generated_logo/user_logo.svg", JSON.stringify(circle, text), (err) => {
+    // fs.writeFile("./generated_logo/user_logo.svg", JSON.stringify(circle, text), (err) => {
     // fs.writeFile("./user_generated_logo/logo.svg", userSelectedItems(...response), (err) => {
-        err ? console.log(err) : console.log("Generated logo.svg");
-    });
+        // err ? console.log(err) : console.log("Generated logo.svg");
+    // });
     // console.log({ ...responseArray[2]});
 })
-
-// // .then((res) => fetch(`https://api.github.com/users/${res.username}`))
-// //   // promises are chained to parse the request for the json data
-// //   .then((res) => res.json())
-// //   // json data is accepted as user and logged to the console
-// //   .then((user) => console.log(user));
-
-// // TODO: Add a comment describing what kind of function this is
-// // It is called a constructor function, which takes in parameters and then...
-// function BlogPost(authorName, title, text, createdOn) {
-//   this.authorName = authorName;
-//   this.title = title;
-//   this.text = text;
-//   this.createdOn = createdOn;
-//   this.comments = [];
-//   this.printMetaData = function () {
-//     console.log(`Created by ${this.authorName} on ${this.createdOn}`);
-//   };
-// }
-
-// // TODO: Add a comment describing the purpose of `.prototype` in this method declaration
-// // Use the prototype to create only one function... Do this to save memory...
-// // Any new BlogPost will reference this prototype
-// // Creates a new function called addComment and pushes the parameter (comment) to the array comments in the BlogPost constructor
-// BlogPost.prototype.addComment = function(comment) {
-//   this.comments.push(comment);
-// };
-
-// const post = new BlogPost(
-//   'John Doe',
-//   'My Second Post',
-//   'Cats are super cute!',
-//   '12/16/2021'
-// );
-
-// // post references the new BlogPost constructor and .addComment is a callback to the function on the prototype
-// post.addComment('Nice post, I like it!');
-
-// // TODO: Add a comment describing what you expect to see printed in the console
-// // You would expext to see post.addComment
-// console.log(post.comments);
-// console.log(post.printMetaData());
-
-// // TODO: Add a comment describing what kind of function this is
-// // It is called a constructor function, which takes in parameters and then...
-// function BlogPost(authorName, title, text, createdOn) {
-//     this.authorName = authorName;
-//     this.title = title;
-//     this.text = text;
-//     this.createdOn = createdOn;
-//     this.comments = [];
-//     this.printMetaData = function () {
-//       console.log(`Created by ${this.authorName} on ${this.createdOn}`);
-//     };
-//   }
-  
-//   // TODO: Add a comment describing the purpose of `.prototype` in this method declaration
-//   // Use the prototype to create only one function... Do this to save memory...
-//   // Any new BlogPost will reference this prototype
-//   // Creates a new function called addComment and pushes the parameter (comment) to the array comments in the BlogPost constructor
-//   BlogPost.prototype.addComment = function(comment) {
-//     this.comments.push(comment);
-//   };
-  
-//   const post = new BlogPost(
-//     'John Doe',
-//     'My Second Post',
-//     'Cats are super cute!',
-//     '12/16/2021'
-//   );
-  
-//   // post references the new BlogPost constructor and .addComment is a callback to the function on the prototype
-//   post.addComment('Nice post, I like it!');
-  
-//   // TODO: Add a comment describing what you expect to see printed in the console
-//   // You would expext to see post.addComment
-//   console.log(post.comments);
-//   console.log(post.printMetaData());
