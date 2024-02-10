ab5df2c53eee2e0f02f0ceed2918aa0361412817
JLP-55 <jack.v.pekin@gmail.com>
2024-02-10 19:34:52 +1100

    Conditional statement to dynamically change the text border colour

--- index.js
+++ index.js
@@ -37,17 +37,27 @@ const userQuestions = [
 inquirer.prompt(userQuestions)
     .then((response) => {
 
-        // Set the variable "shape" to an empty value
-        let shape;
+        let black = response.textColour.toLowerCase();
+    
+        // Conditional statement to check whether the user selected black text.
+        // If so, the text border will be white, otherwise it will be black.
+        if (black === "black") {
+            strokeColour = "white";
+        } else {
+            strokeColour = "black";
+        };
 
+        // Declares the variable "shape" without assigning a value.
+        let shape;
+        
         // Reasign value of shape dependant on the users input.
         if (response.logoShape === "Circle") {
             // Here, shape will equal a new instance of the Circle class
-            shape = new Circle(response.logoColour, response.textColour, response.textContent);
+            shape = new Circle(response.logoColour, response.textColour.toLowerCase(), response.textContent);
         } else if (response.logoShape === "Square") {
-            shape = new Square(response.logoColour, response.textColour, response.textContent);
+            shape = new Square(response.logoColour, response.textColour.toLowerCase(), response.textContent);
         } else {
-            shape = new Triangle(response.logoColour, response.textColour, response.textContent);
+            shape = new Triangle(response.logoColour, response.textColour.toLowerCase(), response.textContent);
         };
 
         //Write to the file.
